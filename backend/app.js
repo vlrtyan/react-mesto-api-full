92e12be95b39d7ef37f311d9f7f304b0c7bcc1be
Valeria Tyan <vlrtyan@yandex.ru>
2022-09-20 03:20:53 +0900

    crush test

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,12 @@ app.use(cors());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', validateUserBody, createUser);
 app.post('/signin', validateAuthentification, login);
 
